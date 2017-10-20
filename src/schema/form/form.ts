a34690f7b4539a0624f7ba85b0671598c7ddf6fc
Hajime TAKAHASHI <cc.sig@icloud.com>
2017-10-21 05:32:13 +0900

    Export FormShema with constructor overloads

--- src/schema/form/form.ts
+++ src/schema/form/form.ts
@@ -3,12 +3,7 @@ import { InputSchema, InputTypeOf } from '../input';
 
 export type Formatter<TInputs extends Record<string, InputSchema>, TOutputs> = (inputs: InputTypeOf<TInputs>) => TOutputs;
 
-export interface FormSchema<TInputs extends Record<string, InputSchema>, TOutputs> {
-  readonly inputs: TInputs;
-  readonly format: Formatter<TInputs, TOutputs>;
-}
-
-export namespace Options {
+export namespace FormSchemaOptions {
   export interface NonFormatting<TInputs extends Record<string, InputSchema>> {
     inputs: TInputs;
     outputs?: never;
@@ -20,28 +15,18 @@ export namespace Options {
   }
 }
 
-export type Options<TInputs extends Record<string, InputSchema>, TOutputs> =
-  | Options.NonFormatting<TInputs>
-  | Options.Formatting<TInputs, TOutputs>;
+export type FormSchemaOptions<TInputs extends Record<string, InputSchema>, TOutputs> =
+  | FormSchemaOptions.NonFormatting<TInputs>
+  | FormSchemaOptions.Formatting<TInputs, TOutputs>;
 
-export interface FormSchemaClass {
-  new <TInputs extends Record<string, InputSchema>, TOutputs>(
-    options: Options.Formatting<TInputs, TOutputs>,
-  ): FormSchema<TInputs, TOutputs>;
-  new <TInputs extends Record<string, InputSchema>>(
-    options: Options.NonFormatting<TInputs>,
-  ): FormSchema<TInputs, InputTypeOf<TInputs>>;
-}
-
-// tslint:disable-next-line max-line-length
-export const FormSchema: FormSchemaClass = class <TInputs extends Record<string, InputSchema>, TOutputs> implements FormSchema<TInputs, TOutputs> {
+export class FormSchema<TInputs extends Record<string, InputSchema>, TOutputs> {
   public readonly inputs: TInputs;
   public readonly format: Formatter<TInputs, TOutputs>;
 
-  constructor(options: Options.Formatting<TInputs, TOutputs>);
-  constructor(options: Options.NonFormatting<TInputs>);
-  constructor({ inputs, outputs = identity }: Options<any, any>) {
+  constructor(options: FormSchemaOptions.Formatting<TInputs, TOutputs>);
+  constructor(options: FormSchemaOptions.NonFormatting<TInputs>);
+  constructor({ inputs, outputs = identity }: FormSchemaOptions<TInputs, any>) {
     this.inputs = inputs;
     this.format = outputs;
   }
-};
+}
--- src/schema/form/index.ts
+++ src/schema/form/index.ts
@@ -1,1 +1,14 @@
-export { FormSchema } from './form';
+import { InputSchema, InputTypeOf } from '../input';
+import { FormSchema as _FormSchema, FormSchemaOptions } from './form';
+
+export interface FormSchemaClass {
+  new <TInputs extends Record<string, InputSchema>, TOutputs>(
+    options: FormSchemaOptions.Formatting<TInputs, TOutputs>,
+  ): _FormSchema<TInputs, TOutputs>;
+  new <TInputs extends Record<string, InputSchema>>(
+    options: FormSchemaOptions.NonFormatting<TInputs>,
+  ): _FormSchema<TInputs, InputTypeOf<TInputs>>;
+}
+
+export type FormSchema<TInputs extends Record<string, InputSchema>, TOutputs> = _FormSchema<TInputs, TOutputs>;
+export const FormSchema: FormSchemaClass = _FormSchema;

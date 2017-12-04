22c9c400a9aad25f0b75c4b049f6d062bcf6e879
Hajime TAKAHASHI <cc.sig@icloud.com>
2017-12-04 23:00:57 +0900

    Fix extension retrieval

--- src/utils/file.spec.ts
+++ src/utils/file.spec.ts
@@ -6,18 +6,22 @@ describe('convertBlobToFile()', () => {
 
     const jpg = convertBlobToFile('foo.jpg')(blob);
     expect(jpg).toBeInstanceOf(File);
+    expect(jpg.name).toEqual('uploaded.jpeg');
     expect(jpg.type).toEqual('image/jpeg');
 
     const jpeg = convertBlobToFile('foo.jpeg')(blob);
     expect(jpeg).toBeInstanceOf(File);
+    expect(jpeg.name).toEqual('uploaded.jpeg');
     expect(jpeg.type).toEqual('image/jpeg');
 
     const png = convertBlobToFile('foo.png')(blob);
     expect(png).toBeInstanceOf(File);
+    expect(png.name).toEqual('uploaded.png');
     expect(png.type).toEqual('image/png');
 
     const gif = convertBlobToFile('foo.gif')(blob);
     expect(gif).toBeInstanceOf(File);
+    expect(gif.name).toEqual('uploaded.gif');
     expect(gif.type).toEqual('image/gif');
   });
 });
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,4 +1,9 @@
 import * as mime from 'mime';
 
-export const convertBlobToFile = (url: string) => (blob: Blob) =>
-  new File([blob], `uploaded.${mime.getExtension(url)}`, { type: mime.getType(url) || undefined });
+export const convertBlobToFile = (url: string) => (blob: Blob) => {
+  const mimeType = mime.getType(url);
+  const extension = mimeType && mime.getExtension(mimeType);
+  const filename = extension ? `uploaded.${extension}` : '';
+
+  return new File([blob], filename, { type: mimeType || undefined });
+};

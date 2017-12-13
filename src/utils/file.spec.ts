c12459ed633f9163583505865d09825f2ca231cd
Hajime TAKAHASHI <cc.sig@icloud.com>
2017-12-13 12:14:33 +0900

    Do not use file constructor due to IE bug

--- src/utils/file.spec.ts
+++ src/utils/file.spec.ts
@@ -5,22 +5,22 @@ describe('convertBlobToFile()', () => {
     const blob = new Blob();
 
     const jpg = convertBlobToFile('foo.jpg')(blob);
-    expect(jpg).toBeInstanceOf(File);
+    expect(jpg).toBeInstanceOf(Blob);
     expect(jpg.name).toEqual('uploaded.jpeg');
     expect(jpg.type).toEqual('image/jpeg');
 
     const jpeg = convertBlobToFile('foo.jpeg')(blob);
-    expect(jpeg).toBeInstanceOf(File);
+    expect(jpeg).toBeInstanceOf(Blob);
     expect(jpeg.name).toEqual('uploaded.jpeg');
     expect(jpeg.type).toEqual('image/jpeg');
 
     const png = convertBlobToFile('foo.png')(blob);
-    expect(png).toBeInstanceOf(File);
+    expect(png).toBeInstanceOf(Blob);
     expect(png.name).toEqual('uploaded.png');
     expect(png.type).toEqual('image/png');
 
     const gif = convertBlobToFile('foo.gif')(blob);
-    expect(gif).toBeInstanceOf(File);
+    expect(gif).toBeInstanceOf(Blob);
     expect(gif.name).toEqual('uploaded.gif');
     expect(gif.type).toEqual('image/gif');
   });
